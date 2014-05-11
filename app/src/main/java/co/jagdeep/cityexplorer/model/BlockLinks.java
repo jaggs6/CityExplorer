79f292afcd513255168b081520759fb47456238d
Jagdeep Nagpal <jagdeep.nagpal@bskyb.com>
2014-05-11 01:20:37 +0100

    optimisation

--- app/src/main/java/co/jagdeep/cityexplorer/model/BlockLinks.java
+++ app/src/main/java/co/jagdeep/cityexplorer/model/BlockLinks.java
@@ -1,26 +1,13 @@
 package co.jagdeep.cityexplorer.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BlockLinks {
-	@JsonProperty("@tag")
-	public String tag;
-
 	@JsonProperty("@id")
 	public String id;
-
-	@JsonProperty("@parent")
-	public String parent;
-
-	@JsonProperty("@categories")
-	public String categories;
-
-	@JsonProperty("@role")
-	public String role;
-
-	@JsonProperty("@type")
-	public String type;
 
 	@JsonProperty("@latitude")
 	public String latitude;
